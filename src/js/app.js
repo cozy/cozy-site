7a35273e6915f7dec12aa58e80f27dc2b93305a8
Julien van der Kluft <julien@minuitsept.com>
2015-11-29 00:36:10 +0100

    Update features fonctionnal behavior

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,32 @@
 $(function() {
 
+  // Features
+  //
+  // Config features screenshot switching
+  var $featuresLink         = $('.featuresList-item-link'),
+      $screenshotsContainer = $('.featuresList-screenshots'),
+      $featuresScreenshot   = $('.featuresList-screenshotOutter');
+
+  // Set height of screenshots container, from the first slide at launch
+  var firstScreenshotHeight = $featuresScreenshot.filter('.active').find('.featuresList-screenshot').height();
+
+  $screenshotsContainer.height(firstScreenshotHeight + 'px');
+
+  $featuresLink.on('click', function() {
+    $featuresLink.filter('.active').removeClass('active');
+    $(this).addClass('active');
+
+    var target = $(this).data('target');
+
+    $('.featuresList-screenshotOutter.active').removeClass('active');
+    $('#feature-' + target).addClass('active');
+
+    return false;
+  });
+
+
+  // Smooth scrolling on click on anchor link
+  //
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
