f6dc8858895abbb48a811a60c7eb05f4075f69ce
martinsam <martin.sam@gmail.com>
2016-02-18 14:05:51 +0100

    add i18n string for title demo - app.html

--- utils/app.js
+++ utils/app.js
@@ -76,7 +76,7 @@ client.get("repos/cozy/cozy-registry/contents/apps", (req, res, body) => {
       log.info(app.slug);
       if (repos_with_demo.hasOwnProperty(app.slug)){
         var demo_url = repos_with_demo[app.slug];
-        var demo_title = `Go to the ${app.slug} demo`;
+        var demo_title = `{{__ 'Go to the ${app.slug} demo' }}`;
         var demo_tag = `<a class="app-demo" title="${demo_title}" href="${demo_url}">Demo</a>`;
       };
 
